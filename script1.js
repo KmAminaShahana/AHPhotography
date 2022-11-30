b65867acc8fa7367fd5c4eed42b400d3d18abd81
Harismitha <68776186+harismitha13@users.noreply.github.com>
2022-11-30 17:10:32 +0530

    load elements
    loading front page
    back to top button
    color placeholder skeleton

--- script1.js
+++ script1.js
@@ -2,7 +2,27 @@
 
 $(document).ready(function(){
 	
+	const showOnPx = 100;
+	const backToTopButton = document.querySelector(".back-to-top")
+
+	const scrollContainer = () => {
+  		return document.documentElement || document.body;
+	};
+	const goToTop = () => {
+		document.body.scrollIntoView({
+		  behavior: "smooth",
+		});
+	  };
+
+	document.addEventListener("scroll", () => {
+	if (scrollContainer().scrollTop > showOnPx) {
+		backToTopButton.classList.remove("hidden")
+	} else {
+		backToTopButton.classList.add("hidden")
+	}
+	})
 	
+
 	var checkout = $('#date-picker').datepicker({
 		beforeShowDay: function(date) {
 		  if (!checkin.datepicker("getDate").valueOf()) {
